572842bd0bd0e849e00e9e2bfc604dd905a0f3b0
pofnor <142336027+pofnor@users.noreply.github.com>
2023-10-30 14:43:45 +0330

    update NotFound

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,6 @@
 const Dashboard = () => {
   return (
-    <div>Dashboard</div>
+    <h1 style={{display:"flex",justifyContent:"center",marginTop:"50px",color:"#CD853F"}}>Dashboard</h1>
   )
 }
 
--- src/Pages/NotFound.jsx
+++ src/Pages/NotFound.jsx
@@ -1,8 +1,15 @@
+import { useEffect } from "react"
+import {useNavigate } from "react-router-dom"
+
 const NotFound = () => {
+  const navigate = useNavigate()
+  useEffect(()=>{
+    setTimeout(() => {
+      navigate("/")      
+    }, 2000);
+  })
   return (
-    <div>
-      NotFound
-      </div>
+    <h1 style={{display:"flex",justifyContent:"center",marginTop:"50px",color:"#CD853F"}}>The Page Not Found</h1>
   )
 }
 
