460fdb36903cfb7c8d43e18f6264129fc89b254a
Ahmad <142336027+pofnor@users.noreply.github.com>
2023-10-30 17:15:27 +0330

    add Dashboard

---
+++ src/Constants/carouselDB.jsx
@@ -0,0 +1,25 @@
+const DB = [
+  {
+    id: 1,
+    image: 'img-hero1.jpg',
+    title: 'The perfect design for your website',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab suscipit dicta nulla. Consequuntur obcaecati officiis, labore doloribus non tempore impedit consequatur ab dolor. Explicabo quam repellendus vero omnis, nisi odio!',
+    link: 'https://www.google.com'
+  },
+  {
+    id: 2,
+    image: 'img-hero2.jpg',
+    title: 'Start Your Future Financial Plan',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab suscipit dicta nulla. Consequuntur obcaecati officiis, labore doloribus non tempore impedit consequatur ab dolor. Explicabo quam repellendus vero omnis, nisi odio!',
+    link: 'https://www.facebook.com'
+  },
+  {
+    id: 3,
+    image: 'img-hero3.jpg',
+    title: 'Enjoy the Difference',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab suscipit dicta nulla. Consequuntur obcaecati officiis, labore doloribus non tempore impedit consequatur ab dolor. Explicabo quam repellendus vero omnis, nisi odio!',
+    link: 'https://www.twitter.com'
+  }
+]
+
+export default DB;
--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,6 +1,30 @@
+import Carousel from 'react-bootstrap/Carousel';
+import DB from '../../Constants/carouselDB'
+import { Link } from 'react-router-dom';
+
 const Dashboard = () => {
   return (
-    <h1 style={{display:"flex",justifyContent:"center",marginTop:"50px",color:"#CD853F"}}>Dashboard</h1>
+    <header>
+      <Carousel>
+        {
+          DB.map(carousel=>{
+            return(
+              <Carousel.Item key={carousel.id}>
+                <img style={{width:"100%"}}
+                  src={carousel.image}
+                  alt={"slide" + carousel.id}
+                />
+                <Carousel.Caption>
+                  <h2>{carousel.title}</h2>
+                  <p>{carousel.description}</p>
+                  <Link style={{textDecoration:"none",color:"whitesmoke",fontSize:"24px"}} to={carousel.link}>Read More</Link>
+                </Carousel.Caption>
+              </Carousel.Item>
+            )
+          })
+        }      
+      </Carousel>
+    </header>
   )
 }
 
