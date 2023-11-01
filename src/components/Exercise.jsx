3157fda1884d58b2ae2054d320552dc75f2f76db
Ahmad <142336027+pofnor@users.noreply.github.com>
2023-11-01 11:51:27 +0330

    complete session

--- src/components/Exercise.jsx
+++ src/components/Exercise.jsx
@@ -1,4 +1,5 @@
 import '../assets/global.css'
+import MultipleLine from './MultipleLine'
 
 const Exercise = (probs) => {
 
@@ -6,13 +7,12 @@ const Exercise = (probs) => {
     <article className="primary-container">
       <div className="primary-question">
         <h1 className='primary-h1'>{probs.questionHeader}</h1>
-        <p className='primary-p'>{probs.question}</p>
-      </div>
-      {probs.answer ? 
+        <MultipleLine code={probs.question} />
+      </div>      
       <div className="primary-answer">
         <h1 className='primary-h1 color-lightgray'>{probs.answerHeader}</h1>
-        <p className='primary-p color-white'>{probs.answer}</p>
-      </div> : <></>}
+        <MultipleLine isAnswer={true} code={probs.answer} />
+      </div>
     </article>
   )
 }
---
+++ src/components/MultipleLine.jsx
@@ -0,0 +1,14 @@
+import '../assets/global.css'
+
+const MultipleLine = ({code,isAnswer=false}) => {    
+  return (
+    <>
+    {code.map(line=>{      
+      return(<p key={line} className={isAnswer ? 'primary-p color-white' : 'primary-p'}>{line}</p>)
+    })
+    }
+    </>
+  )
+}
+
+export default MultipleLine
