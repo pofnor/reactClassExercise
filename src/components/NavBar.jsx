afa6892d9f1fdcca08ea0905cedadc3311f40222
Ahmad <142336027+pofnor@users.noreply.github.com>
2023-11-04 17:02:58 +0330

    dynamic session in navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,8 +3,28 @@ import Container from 'react-bootstrap/Container'
 import Nav from 'react-bootstrap/Nav'
 import Navbar from 'react-bootstrap/Navbar'
 import NavDropdown from 'react-bootstrap/NavDropdown'
+import { sessions } from '../data/sessionDB'
 
 const NavBar = () => {
+  function totalSession (sessions) {
+    const sessionNumbers = []
+    for (const i in sessions) {
+      sessionNumbers.push(sessions[i].session)
+    }
+
+    const totalLink = Math.max(...sessionNumbers)
+    const result = []
+    for (let i = 0; i < totalLink; i++) {
+      result.push({
+        href: '/session/' + (i + 1),
+        linkname: 'Session ' + (i + 1)
+      })
+    }
+    return result
+  }
+
+  const Links = totalSession(sessions)
+
   useEffect(() => {
     console.log("NavBar Mount or Update,Why the react-bootstrap/Navbar don't use Link ???")
   }, [])
@@ -19,16 +39,9 @@ const NavBar = () => {
             <Nav.Link href="/">Home</Nav.Link>
             <Nav.Link href="/search">Search</Nav.Link>
             <NavDropdown title="Session" id="basic-nav-dropdown">
-              <NavDropdown.Item href="/session/1">Session 1</NavDropdown.Item>
-              <NavDropdown.Item href="/session/2">Session 2</NavDropdown.Item>
-              <NavDropdown.Item href="/session/3">Session 3</NavDropdown.Item>
-              <NavDropdown.Item href="/session/4">Session 4</NavDropdown.Item>
-              <NavDropdown.Item href="/session/5">Session 5</NavDropdown.Item>
-              <NavDropdown.Item href="/session/6">Session 6</NavDropdown.Item>
-              <NavDropdown.Item href="/session/7">Session 7</NavDropdown.Item>
-              <NavDropdown.Item href="/session/8">Session 8</NavDropdown.Item>
-              <NavDropdown.Item href="/session/9">Session 9</NavDropdown.Item>
-              <NavDropdown.Item href="/session/10">Session 10</NavDropdown.Item>
+              {Links.map(link => {
+                return (<NavDropdown.Item href={link.href}>{link.linkname}</NavDropdown.Item>)
+              })}
               {/* <NavDropdown.Divider />
               <NavDropdown.Item href="#action/3.4">
                 Separated link
--- src/data/sessionDB.js
+++ src/data/sessionDB.js
@@ -11,63 +11,63 @@ export const sessions = [
   {
     session: '3',
     questionHeader: 'TEMP Reminder',
-    question: ['Store','Broadcasting from top to bottom','Redux','Redux/dispatch','React/dispatch','Context'],
+    question: ['Store', 'Broadcasting from top to bottom', 'Redux', 'Redux/dispatch', 'React/dispatch', 'Context', 'NavBar Mount or Update,Why the react-bootstrap/Navbar don`t use Link ???']
   },
   {
     session: '2',
     questionHeader: 'React Router Dom',
-    question: ['line1','line2','line3'],
+    question: ['line1', 'line2', 'line3'],
     answerHeader: 'header',
-    answer: ['💻 (App.js)','line1','line2','line3'],
+    answer: ['💻 (App.js)', 'line1', 'line2', 'line3']
   },
   {
     session: '2',
     questionHeader: 'Babel',
     question: ['what is Babel?'],
     answer: ['Put in next-gen JavaScript and Get browser-compatible Javascript out',
-      'because of this, in older version of react , use the import react'],
+      'because of this, in older version of react , use the import react']
   },
   {
     session: '1',
     questionHeader: 'Class Component re-renders',
-    question: ['React.Component','React.PureComponent'],
-    answer: ['PureComponent skipping unnecessary re-renders for class components'],
+    question: ['React.Component', 'React.PureComponent'],
+    answer: ['PureComponent skipping unnecessary re-renders for class components']
   },
   {
     session: '1',
     questionHeader: 'Function Components',
-    question: ['hooks','useState','useEffect','compare to Class Components'],
+    question: ['hooks', 'useState', 'useEffect', 'compare to Class Components'],
     answer: [
       '💻 (App.js)',
       'import { useEffect, useState } from "react"',
       'function App() {',
-        'const [toggle1,setToggle1] = useState(false)',
-        'const [toggle2,setToggle2] = useState(false)',
-        'useEffect(()=>{console.log("like componentDidMount() or componentDidUpdate()")})',
-        'useEffect(()=>{console.log("like componentDidMount()")},[])',
-        'useEffect(()=>{console.log("like componentDidUpdate() just for toggle1")},[toggle1])',
-        'console.log("render")',
-        'return (',
-          '<div>',
-            '<button onClick={()=>setToggle1(!toggle1)}>Toggle1</button>',
-            '<button onClick={()=>setToggle2(!toggle2)}>Toggle2</button>',
-          '</div>',
-        ')',
+      'const [toggle1,setToggle1] = useState(false)',
+      'const [toggle2,setToggle2] = useState(false)',
+      'useEffect(()=>{console.log("like componentDidMount() or componentDidUpdate()")})',
+      'useEffect(()=>{console.log("like componentDidMount()")},[])',
+      'useEffect(()=>{console.log("like componentDidUpdate() just for toggle1")},[toggle1])',
+      'console.log("render")',
+      'return (',
+      '<div>',
+      '<button onClick={()=>setToggle1(!toggle1)}>Toggle1</button>',
+      '<button onClick={()=>setToggle2(!toggle2)}>Toggle2</button>',
+      '</div>',
+      ')',
       '}',
-      'export default App'      
-    ],
+      'export default App'
+    ]
   },
   {
     session: '1',
     questionHeader: 'Idioms',
     question: ['Tree shaking'],
-    answer: ['Removing unused code'],
+    answer: ['Removing unused code']
   },
   {
     session: '1',
     questionHeader: 'Idioms',
     question: ['HMR'],
-    answer: ['Hot Module Replacement'],
+    answer: ['Hot Module Replacement']
   },
   {
     session: '1',
@@ -79,8 +79,8 @@ export const sessions = [
       'Vite uses Rollup and esbuild under the hood; esbuild is used for pre-bundling while Rollup is used for Vite’s production builds.',
       'Nextjs use Turbopack',
       'Turbopack is The Rust-powered successor to Webpack',
-      'The Turbopack team view it as the natural successor to Webpack (it`s built by Vercel team members who originally built Webpack)',
-    ],
+      'The Turbopack team view it as the natural successor to Webpack (it`s built by Vercel team members who originally built Webpack)'
+    ]
   },
   {
     session: '2',
@@ -98,7 +98,7 @@ export const sessions = [
       'import { ajax } from "./utils";',
       'const query = "Rollup";',
       'ajax(`https://api.example.com?search=${query}`).then(handleResponse);'
-    ],
+    ]
   },
   {
     session: '1',
@@ -108,27 +108,27 @@ export const sessions = [
       '💻 (App.js)',
       'import React from "react";',
       'class App extends React.Component {',
-        'constructor(){',
-          'super()',
-          'this.state={toggle:true}',
-          'console.log("here in constructor")',
-        '}',
-        'componentDidMount(){',
-          'console.log("here in componentDidMount")',
-        '}',
-        'componentDidUpdate(){',
-            'console.log("here in componentDidUpdate")',
-            '}',
-            'render() {',
-            'console.log("here in render")',
-            'return(',
-            '<>',
-            '<button onClick={()=>{this.setState(()=>({toggle:this.state.toggle}))}}>toggle</button>',
-            '</>',
-            ')',
-            '}',        
+      'constructor(){',
+      'super()',
+      'this.state={toggle:true}',
+      'console.log("here in constructor")',
       '}',
-      'export default App;',
-    ],
-  },
-];
+      'componentDidMount(){',
+      'console.log("here in componentDidMount")',
+      '}',
+      'componentDidUpdate(){',
+      'console.log("here in componentDidUpdate")',
+      '}',
+      'render() {',
+      'console.log("here in render")',
+      'return(',
+      '<>',
+      '<button onClick={()=>{this.setState(()=>({toggle:this.state.toggle}))}}>toggle</button>',
+      '</>',
+      ')',
+      '}',
+      '}',
+      'export default App;'
+    ]
+  }
+]
